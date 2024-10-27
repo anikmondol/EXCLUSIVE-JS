c5927c58f89b196af65abee012734b1995a2e569
anikmondol <anikmondol558363@gmail.com>
2024-10-27 11:43:26 +0600

    E-Commerce-Js website project added Animations using ScrollReveal js web.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+// countdown
+
 let countDownDate = new Date("Jan 1, 2025 15:37:25").getTime();
 
 // Function to update the countdown
@@ -49,6 +51,7 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+// trending countdown
 
 const targetDate = new Date();
 targetDate.setDate(targetDate.getDate() + 5); 
@@ -82,3 +85,41 @@ function updateCountdown() {
 
 // Update the countdown every second
 const countdownInterval = setInterval(updateCountdown, 1000);
+
+
+// Animations
+ScrollReveal().reveal(".top_nav", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+});
+
+ScrollReveal().reveal(".nav", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".header", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".section", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  duration: 900,
+  delay: 100,
+});
+
+ScrollReveal().reveal(".footer", {
+  origin: "bottom",
+  distance: "20px",
+  opacity: 0,
+  duration: 900,
+  delay: 100,
+});
