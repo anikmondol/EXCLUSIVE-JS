118a0fce50b63f78c10a18049ad49534a2b23fd6
anikmondol <anikmondol558363@gmail.com>
2024-10-29 19:34:49 +0600

    E-Commerce-Js website project check product_details page desing done with full resposive.

--- js/product_details.js
+++ js/product_details.js
@@ -75,7 +75,7 @@ var swiper = new Swiper(".mySwiper", {
             spaceBetween: 10,
         },
         768: {
-            slidesPerView: 3,
+            slidesPerView: 4,
             spaceBetween: 10,
         },
         1024: {
